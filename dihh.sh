c7c86ae513dd5161dede228d3898d5ab149ba830
loraleetiara <loraleetiara@gmail.com>
2024-08-08 20:20:08 +0500

    feat: something changed #1

---
+++ dihh.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+DAYS_BACK=700
+COMMITS_PER_DAY=4000
+
+echo "Погнали крутить счётчик для кота..."
+
+for ((i=$DAYS_BACK; i>=0; i--)); do
+    CURRENT_DATE=$(date -v-"${i}"d +"%Y-%m-%d %H:%M:%S")
+    
+    echo "Забиваем дату: $CURRENT_DATE"
+    
+    for ((j=1; j<=$COMMITS_PER_DAY; j++)); do
+        echo "flex $i $j" >> borsch.txt
+        GIT_AUTHOR_DATE="$CURRENT_DATE" GIT_COMMITTER_DATE="$CURRENT_DATE" git commit -am "feat: something changed #$j" --quiet
+    done
+done
+
+echo "Локально упаковано! Привязываем репу и пушим..."
